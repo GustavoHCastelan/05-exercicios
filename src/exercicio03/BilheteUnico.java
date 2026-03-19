71beb03bbde12fa47f0ad51df65c72a0f5b4d997
GustavoHCastelan <gucastelanfiap@gmail.com>
2026-03-19 11:47:55 -0300

    Add files via upload

--- src/exercicio03/BilheteUnico.java
+++ src/exercicio03/BilheteUnico.java
@@ -5,13 +5,29 @@ import java.util.Random;
 public class BilheteUnico {
     int numero;
     double saldo;
-    final static double tarifaBase = 5.4;
+    final static double TARIFA = 5.4;
     Usuario usuario;
 
     public BilheteUnico (Usuario usuario) {
         Random random = new Random();
         this.numero = random.nextInt(1000, 9999);
         this.usuario = usuario;
+    }
+
+    public void carregar (double valor) {
+        saldo += valor;
+    }
+
+    public boolean passarNaCatraca () {
+        double valor = TARIFA / 2;
+        if (usuario.tipoTarifa.equalsIgnoreCase("comum")) {
+            valor = TARIFA;
+        }
+        if (saldo < valor){
+            return false;
+        }
+        saldo -= valor;
+        return true;
     }
 
 }
