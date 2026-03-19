5614f50157bf9d1c9770f8301037ce7ee2f80b14
GustavoHCastelan <gucastelanfiap@gmail.com>
2026-03-19 11:48:34 -0300

    Add files via upload

--- src/exercicio03/Main.java
+++ src/exercicio03/Main.java
@@ -11,22 +11,27 @@ public class Main {
         int opcao;
 
         do {
+            System.out.println("*********************************************");
             System.out.println("1. Cadastrar bilhete");
             System.out.println("2. Carregar bilhete");
             System.out.println("3. Passar na catraca");
             System.out.println("4. Finalizar");
+            System.out.println();
+            System.out.print("Escolha uma opção: ");
             opcao = sc.nextInt();
 
-            for (int i = 0; i < bilhete.length; i++) {
-                switch (opcao){
+            switch (opcao){
                     case 1 -> cadastrar();
-                }
-
+                    case 2 -> carregar();
+                    case 3 -> passarNaCatraca();
+                    case 4 -> System.out.println("Obrigado por usar nosso sistema! Até breve!");
+                    default -> System.out.println("Opção inválida!");
             }
 
         }while(opcao != 4);
 
     }
+
 
     private static void cadastrar() {
         String nome;
@@ -38,18 +43,19 @@ public class Main {
             nome = sc.next();
             System.out.print("CPF --> ");
             cpf = sc.nextLong();
-            System.out.print("Tipo de Tarifa (estudante | professor | normal) --> ");
+            System.out.print("Tipo de Tarifa (estudante | professor | comum) --> ");
             tipoTarifa = sc.next();
-
+            System.out.println();
             //Usuario usuario = new Usuario(nome, cpf, tipoTarifa);
             bilhete[index] = new BilheteUnico(new Usuario(nome, cpf, tipoTarifa));
             index++;
         }else {
             System.out.println("Erro ao gerar o bilhete! Procure um posto de atendimento.");
+            System.out.println();
         }
     }
 
-    public BilheteUnico pesquisar() {
+    public static BilheteUnico pesquisar() {
         long cpf;
         System.out.println("Qual o CPF para pesquisa?");
         System.out.print("-> ");
@@ -60,10 +66,12 @@ public class Main {
                 return bilhete[i];
             }
         }
+        System.out.println("CPF não encontrado!");
+        System.out.println();
         return null;
     }
 
-    public void carregar() {
+    public static void carregar() {
         double valor;
         BilheteUnico bilheteEncontrado = pesquisar();
 
@@ -74,5 +82,16 @@ public class Main {
 
             bilheteEncontrado.carregar(valor);
         }
+    }
+
+    private static void passarNaCatraca() {
+        BilheteUnico bilheteUnico = pesquisar();
+        if (bilheteUnico != null) {
+            if (!bilheteUnico.passarNaCatraca()){
+                System.out.println("Saldo insuficinete");
+            }
+        }
+        System.out.println("Saldo atual R$ "+ bilheteUnico.saldo);
+        System.out.println();
     }
 }
